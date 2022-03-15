0587c9beb9ac0e3292ad47b474d980f8614fdf39
llama <progllama.rust@gmail.com>
2022-03-16 01:24:25 +0900

    [ADD] finish easy processing.

--- src/index.js
+++ src/index.js
@@ -4,17 +4,39 @@ const { program } = require('commander');
 const fs = require('fs');
 const { exit } = require('process');
 
-program
-    .option('--path <path>')
-    .parse(process.argv);
+async function main() {
+    program
+        .option('--path <path>')
+        .parse(process.argv);
 
-const opts = program.opts();
+    const opts = program.opts();
 
-console.log(opts.path);
+    console.log(opts.path);
 
-if (!fs.existsSync(opts.path)) {
-    console.log("指定されたパスがありません。");
-    exit();
+    if (!fs.existsSync(opts.path)) {
+        console.log("指定されたパスがありません。");
+        exit();
+    }
+
+    const img = await sharp(opts.path)
+    const raw = await img.raw();
+    const meta = await img.metadata();
+
+    console.log(meta);
+
+
+    const reversed = (await raw.toBuffer()).reverse();
+
+    sharp( reversed , 
+        {
+            raw:{
+                width:meta.width,
+                height:meta.height,
+                channels:3,
+            }
+        } )
+        .rotate(180)
+        .toFile('test1.jpeg');
 }
 
-sharp(opts.path);
+main();
